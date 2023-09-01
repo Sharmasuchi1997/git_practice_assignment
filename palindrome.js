5d8af633a92fb21a84fdb2cff113a7de70061e67
developer-fardin786 <developerfardin2.0@gmail.com>
2023-09-01 21:26:08 +0530

    solved all bugs

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,13 @@
 let str="naman"
 
+let str2=""
+
 for(let i=str.length-1; i>=0; i--)
 {
-	if(str==str[i])
-	{
-	console.log("Palindrome")
-	}
-	else
-	{
-	console.log("Not a Palindrome")
-	}
+ str2+=str[i]
+}
+if(str==str2){
+	console.log("palindrome")
+}else{
+	console.log("not palindrome")
 }
--- prime.js
+++ prime.js
@@ -1,17 +1,14 @@
-let num=5;
-let count=0
-for(let i=0; i<num.length; i++)
-{
-	if(num%i==0)
-	{
-	count++}
+let num = 5;
+let count = 0
+for (let i = 0; i <= num.length; i++) {
+	if (num % i == 0) {
+		count++
+	}
 }
-	
-if(count==2)
-{
-console.log("Prime No)
+
+if (count == 2) {
+	console.log("Prime number" )
 }
-else
-{
-console.log("Not Prime No")
+else {
+	console.log("Not Prime number")
 }
